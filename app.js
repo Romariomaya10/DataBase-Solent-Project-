aea1b7bedc2dc3970392cebd995522dd0f66d84c
Romariomaya10 <79513454+Romariomaya10@users.noreply.github.com>
2022-05-30 11:20:49 +0100

    Add files via upload
    adding the files to the git repo

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const connection = require("./DataBase/connection.js");
+const bodyparser = require('body-parser');
+const path = require('path')
+
+const EmployeeRouter = require('./employee/employeeController');
+
+const app = express();
+
+
+
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+
+app.use('/',EmployeeRouter);
+
+
+connection
+    .authenticate()
+    .then(()=> {
+        console.log("conexao feita");
+    })
+    .catch((erro)=>{console.log(erro)});
+
+app.get('/', (req, res)=>{
+  res.render('index.ejs')
+})
+app.listen(8080, ()=>
+    {
+        console.log("funcionando");
+        
+    });
+    
+
+
